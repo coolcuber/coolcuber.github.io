acacf39aa8d546a334a516e676150f1e6806cff6
Ross P. George <mrrosspgeorge@gmail.com>
2018-09-26 20:03:27 -0400

    Design Changes

--- mathmis.js
+++ mathmis.js
@@ -5,3 +5,23 @@ function getx(int x, int width, int max) {
 function gety(int y, int height, int max) {
 	return max*((2*y/height)+1);
 }
+
+/* function drawinit(var ctx) {
+	int w = ctx.width;
+	int h = ctx.height;
+	ctx.moveTo(w/2, 0);
+	ctx.lineTo(w/2, h);
+	ctx.moveTo(0, h/2);
+	ctx.lineTo(w, h/2);
+	ctx.stroke();
+} */
+
+var canvas = document.getElementById("maingraph")
+int w = canvas.width;
+int h = canvas.height;
+
+
+function startup() {
+	
+
+document.addEventListener("DOMContentLoaded", startup());
